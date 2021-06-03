89e13ec29164118ab2fca7ad5179f273864aed96
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-03 17:33:46 +0530

    Welcome To The Generic Program

---
+++ src/main/java/com/magic/generics/findMaximum.java
@@ -0,0 +1,9 @@
+package com.magic.generics;
+
+public class findMaximum {
+
+    public static void main(String[] args) {
+       System.out.println("Welcome To Generic Program To Find Maximum");
+    }
+    }
+
